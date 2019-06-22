1309a805e3527a73b3df1d3feb074582fdbe4b4d
Nico Loubet <44452700+nicoloubet@users.noreply.github.com>
2019-06-22 19:22:06 -0400

    Add files via upload

---
+++ Github/Solutions/RemoveDuplicateCharacter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DuplicateNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 3, 4, 1, 1, 2, 5 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        Console.Write(arr[i]);
+                        Console.ReadLine();
+                    }
+                }
+            }
+
+
+
+        }
+
+    }
+}
