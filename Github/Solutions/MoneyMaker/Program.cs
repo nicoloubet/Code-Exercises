6705279094159a05402206e6f396ae16cab3473e
Nico Loubet <44452700+nicoloubet@users.noreply.github.com>
2019-06-22 18:41:37 -0400

    Add files via upload

---
+++ Github/Solutions/MoneyMaker/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Welcome to Money Maker!");
+
+            Console.WriteLine("Enter an amount to convert to coins:");
+            string totalAsString = Console.ReadLine();
+            int totalAsInt = int.Parse(totalAsString);
+
+            int bronzeValue = 1;
+            int silverValue = 5;
+            int goldValue = 10;
+
+            int currGoldCoin = totalAsInt / goldValue;
+            int remainder = totalAsInt % goldValue;
+
+            int currSilverCoin = remainder / silverValue; 
+           
+            int newRemainder = remainder % silverValue;
+
+            int currBronzeCoin = newRemainder/ bronzeValue;
+            
+
+
+            Console.WriteLine($"{totalAsInt} is eqaul to... Gold coins: {currGoldCoin} Silver coins: {currSilverCoin} Bronze coins: {currBronzeCoin}");
+            Console.ReadLine();
+
+        }
+    }
+}
