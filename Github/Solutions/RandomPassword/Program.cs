b30fa2b41cbb06e36e12ec4027f3a7bb773db06d
Nico Loubet <44452700+nicoloubet@users.noreply.github.com>
2019-06-22 19:25:59 -0400

    Add files via upload

---
+++ Github/Solutions/RandomPassword/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RandomPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letters = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%";
+            Random rnd = new Random();
+            int maxLen = 12;
+            string password = "";
+
+            while (password.Length < maxLen)
+            {
+                password += letters[rnd.Next(letters.Length)];
+            }
+
+            Console.WriteLine($"Random Password: { password}");
+            Console.ReadKey();
+        }
+    }
+}
