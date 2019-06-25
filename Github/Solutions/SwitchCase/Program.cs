8c31e2b46c3347243c6b1b4fb4cdb870a1bd201a
Nico Loubet <44452700+nicoloubet@users.noreply.github.com>
2019-06-24 23:59:45 -0400

    Add files via upload

---
+++ Github/Solutions/SwitchCase/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a movie genre");
+            string genre = Console.ReadLine();
+
+
+            switch (genre)
+            {
+                case "Horror":
+                    Console.WriteLine("Childs Play");
+                    Console.ReadLine();
+                    break;
+                case "Comedy":
+                    Console.WriteLine("Step Brothers");
+                    Console.ReadLine();
+                    break;
+                case "Science Fiction":
+                    Console.WriteLine("Pandorum");
+                    Console.ReadLine();
+                    break;
+                case "Action":
+                    Console.WriteLine("Taken");
+                    Console.ReadLine();
+                    break;
+                case "Drama":
+                    Console.WriteLine("Mystic River");
+                    Console.ReadLine();
+                    break;
+
+                default:
+                break;
+            }
+        }
+    }
+}
